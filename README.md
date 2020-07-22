e29aab46d76c6bd1bed17e0c9d1b1f77743b9076
michelle yu <y.michelle03@gmail.com>
2020-07-22 01:15:42 -0400

    created barebones for my part

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # TTNY2020
 Site for TechTogether NY 2020, built by Technical team
+
+TO-DOs:
+
+General
+- Color scheme
+
+Tentative Schedule
+- Choose icon for workshops
+
+Site Content
+- Refer to #website-planning channel in Slack
+
+
+EVENT CATEGORIES
+- Universal #51DBF6 (red)
+- Workshop #808CFB (purple)
+- Panel #E73F33 (blue)
+- Activity #FC7361 (peach)
+
+Workshop Levels
+Icon: Star, Computer, Mascot, etc.
+- Beginner (1)
+- Intermediate (2)
+- Advanced (3)
+
+Questions for Kala
+- Font color + choice
+- Section title colors
+- Button colors
+- Button hover colors
+- Tentative Schedule colors
+- Description for Tentative Schedule
+- Width of table
+- Placement and choice of workshop level icons
+- FAQ hover colors
+- FAQ active colors
---
+++ js/main.js
@@ -0,0 +1,109 @@
+// construct HTML for tentative schedule by reading JSON file with schedule information
+$(document).ready (function() {
+	// create 3 tables, one for each day
+	for (let i=1; i <= 3; i++) {
+		let json_file = `json-files/schedule-day${i}.json`; // json file with event data for specified day
+		let day = `day${i}`;
+		let classes = "accordion";
+		if(i==1) { classes += " active"; } 
+		let html = `<div class="${classes}" id="${day}">`;
+
+		$.getJSON(json_file, function(data) {
+			html = build_schedule(data, html, i) + '</div>'; // call method to build table
+			document.getElementById("schedule-content").innerHTML += html;
+		});	
+	}
+});
+
+// build tentative schedule table - data is contained in json file 
+function build_schedule(data, html, day) {
+	for (let i=0; i < data.length; i++) {
+		let heading = `heading${day}${i}`;
+		let data_target = `collapse${day}${i}`;
+		
+		html+=`<div class="card"> <div class="card-header" id="${heading}">`;
+		html+=`<table class="table" data-toggle="collapse" data-target="#${data_target}" aria-expanded="true">`;
+
+		// column with date/time and event category
+		html+=`<tbody><tr class="${data[i].Class}">`;
+		html+=`<th scope="row">${data[i].Time}<br><span>${data[i].Category}</span></th>`;
+
+		// column with event title and location
+		html+=`<td colspan="2" class="event"><span> ${data[i].Title} </span><br>`;
+		html+=`<span> ${data[i].Location} </span></td>`;
+
+		// add level icons for workshops only
+		if (data[i].Class === 'wshop') {
+			html +='<td class="level text-left" style="vertical-align: middle;">';
+			for (let j=1; j<=data[i].Level; j++) {
+				html +='<i class="fa fa-star"></i>';
+			}
+			html += '</td>';
+		}
+
+		// column with calendar icon
+		html+=`<td class="cal text-center" style="vertical-align: middle;""><a target="_blank" href="${data[i].GCal_Event}">`;
+		html+='<i class="fa fa-calendar"></i></a>';
+		html+='</td></tr></tbody></table></div>';
+		html+=`<div id="${data_target}" class="collapse" aria-labelledby="${heading}" data-parent="#day${day}">`;
+
+		// card body with event description
+		html+=`<div class="card-body ${data[i].Class}"> ${data[i].Description} </div></div></div>`;
+	}
+	return html;
+}
+
+// construct HTML for tentative schedule by reading JSON file with FAQs information
+$(document).ready (function() {
+	$.getJSON("json-files/faqs.json", function(data) {
+		let left_data = data.FAQs_Left;		// array for FAQs in left column
+		let right_data = data.FAQs_Right;	// array for FAQs in right column
+
+		let html = build_faqs(left_data, "L") + build_faqs(right_data, "R");	// call methods to build FAQ cards
+		document.getElementById("faqs-content").innerHTML = html;
+	});
+});
+
+// build FAQs section - data is json file, col is L (left col) or R (right col)
+function build_faqs(data, col) {
+	let data_parent = `faq-${col}cards`;
+	let html = `<div id="${data_parent}" class="col-md-6 col-sm-6 col-xs-6">`;
+
+	for (let i=0; i < data.length; i++) {
+		
+		let heading = `heading${col}${i}`;
+		let data_target = `collapse${col}${i}`;
+
+		html += '<div class="card text-left">';
+
+		// question on card header
+		html += `<div class="card-header" id="${heading}">`;
+		html += `<button class="btn" type="button" data-toggle="collapse" data-target="#${data_target}" aria-expanded="false">`
+		html += `${data[i].Question}<i class="fa fa-caret-down rotate-icon"></i></button></div>`;
+
+		// answer on card body
+		html += `<div id="${data_target}" class="collapse" aria-labelledby="${data_target}" data-parent="#${data_parent}">`;
+		html += `<div class="card-body"> ${data[i].Answer} </div></div></div>`;	
+	}
+
+	html += '</div>';
+	return html;
+}
+
+
+// response to user clicking tab (day) name in tentative schedule
+var btns = document.querySelectorAll(".day-btn");
+for (var i=0; i<btns.length; i++) {
+	btns[i].addEventListener("click", function(event) {
+		// inactivates currently-active button and content
+		let target = event.target;
+		let active_elems = document.querySelectorAll(".day-btn.active, #schedule-content .active");
+		for(let i=0; i<active_elems.length; i++)
+			active_elems[i].classList.remove("active");
+
+		// activates clicked button and its respective content
+		target.className += " active";
+		let btn_name = target.getAttribute("data-tab");
+		document.getElementById(btn_name).className += " active";
+	});
+}
